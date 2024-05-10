a746a478087a79e9dfd7faca1038f22a1fdca9e8
manufac-kjaiswal <kjaiswal@manufacanalytics.com>
2024-05-10 11:11:15 +0530

    API integration and card component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,14 @@
 import React from "react";
 import "./App.css";
+import GithubRepos from "./components/github-repo";
 
 function App() {
-  return <div className="App">Hello world</div>;
+  return (
+    <>
+      <div className="heading">GitHub Repository Search</div>
+      <GithubRepos />
+    </>
+  );
 }
 
 export default App;
---
+++ src/components/github-repo/index.js
@@ -0,0 +1,91 @@
+import React, { useEffect, useState, useRef } from "react";
+import { FaRegStar } from "react-icons/fa6";
+import { FiEye } from "react-icons/fi";
+import "./index.css";
+
+function GithubRepos() {
+  const [query, setQuery] = useState("");
+  const [results, setResults] = useState([]);
+  const debounceTimer = useRef();
+
+  const handleChange = (event) => {
+    setQuery(event.target.value);
+  };
+
+  useEffect(() => {
+    if (query.trim().length > 0) debounceSearch();
+  }, [query]);
+
+  const fetchReposData = async () => {
+    try {
+      const response = await fetch(`https://api.github.com/search/repositories?q=${query}`);
+      const data = await response.json();
+      setResults(data.items);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  const debounceSearch = () => {
+    clearTimeout(debounceTimer.current);
+    debounceTimer.current = setTimeout(() => {
+      fetchReposData();
+    }, 500);
+  };
+
+  const handleKeyPress = (event) => {
+    event.preventDefault();
+    if (event.key === "Enter" && query.trim() !== "") {
+      clearTimeout(debounceTimer.current);
+      fetchReposData();
+    }
+  };
+
+  return (
+    <div className="container">
+      <input
+        type="text"
+        placeholder="Search repositories..."
+        value={query}
+        onChange={handleChange}
+        onKeyUp={handleKeyPress}
+      />
+      <div className="results">
+        {Array.isArray(results) &&
+          results.map((repo) => (
+            <div key={repo.id} className="card">
+              <div className="avatar">
+                <img src={repo.owner.avatar_url} alt="Avatar" loading="lazy" />
+              </div>
+              <div className="text-xl word-break">{repo.full_name}</div>
+              <div className="vertical-align description">{repo.description}</div>
+              <div className="description">Language: {repo.language ? repo.language : "..."}</div>
+              <a href={repo.html_url} target="_blank" rel="noreferrer" className="github">
+                View on GitHub
+              </a>
+              <hr />
+              <div className="display-flex">
+                <div className="h-8" title="Stars">
+                  <span className="icon">
+                    <FaRegStar size={20} />
+                  </span>
+                  {repo.stargazers_count}
+                </div>
+                <div className="h-8" title="Score">
+                  Score: {repo.score}
+                </div>
+                <div className="h-8" title="Watchers count">
+                  <span className="icon">
+                    <FiEye size={20} />
+                  </span>
+                  {repo.watchers_count}
+                </div>
+              </div>
+            </div>
+          ))}
+      </div>
+    </div>
+  );
+}
+
+export default GithubRepos;
